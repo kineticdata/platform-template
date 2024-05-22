f4181c496b22b3a9dc7af865b02004f5b2893a40
Travis Wiese <travis.wiese246@gmail.com>
2024-05-22 13:35:11 -0500

    Updated notes and gitignore

--- export.rb
+++ export.rb
@@ -22,6 +22,9 @@
     SUBMISSIONS_TO_EXPORT:
     - datastore: true
       formSlug: <FORM_SLUG>
+    - datastore: false
+      kappSlug: <KAPP_SLUG>
+      formSlug: <FORM_SLUG>
 
     REMOVE_DATA_PROPERTIES:
     - createdAt
@@ -51,6 +54,7 @@ require 'logger'      #For System Logging
 require 'json'
 require 'optparse'    #For argument parsing
 require 'kinetic_sdk' 
+# Note you may need to run "Gem install Kinetic_sdk"
 require 'Find'        #For config list building
 require 'io/console'  #For password request
 require 'base64'      #For pwd encoding
